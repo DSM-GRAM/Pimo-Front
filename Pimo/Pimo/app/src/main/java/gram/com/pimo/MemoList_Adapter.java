acfefa57e3dad4962b34cc9fcd2a530ca05bca9b
sys8517 <sys8517@naver.com>
2017-11-07 23:25:24 +0900

    add MemoList

--- Pimo/Pimo/app/src/main/java/gram/com/pimo/MemoList_Adapter.java
+++ Pimo/Pimo/app/src/main/java/gram/com/pimo/MemoList_Adapter.java
@@ -1,21 +1,46 @@
 package gram.com.pimo;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import java.util.List;
 
-import static android.os.Build.VERSION_CODES.M;
-
-/**
- * Created by dsm2017 on 2017-11-07.
- */
-
-
-     public class MemoList_Adapter extends RycyclerView.Adapter<MemoList_Adapter.ViewHolder> {
+     public class MemoList_Adapter extends RecyclerView.Adapter<MemoList_Adapter.ViewHolder> {
 
     private List<MemoList_Adapter> memoList;
-    private int itemLayout;
+         private int itemLayout;
 
-    public MemoList_Adapter(List<MemoList_Adapter> items, int itemLayout){
-        this.memoList = items;
-        this.itemLayout = itemLayout;
+         public MemoList_Adapter(List<MemoList_Adapter> items, int itemLayout) {
+             this.memoList = items;
+             this.itemLayout = itemLayout;
+         }
+
+
+         @Override
+         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+             View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.memolist_item, parent,false);
+             return new ViewHolder(view);
+         }
+
+         @Override
+         public void onBindViewHolder(ViewHolder holder, int position) {
+             holder.textView.setText("ddd");
+         }
+
+         @Override
+         public int getItemCount() {
+             return memoList.size();
+         }
+
+         class ViewHolder extends RecyclerView.ViewHolder {
+             //TextView textView 뷰 객체를 선언을 해준다.
+             TextView textView;
+             public ViewHolder(View itemView) {
+                 super(itemView);
+                 //textView=itemView.findbyid() 아이디를 찾아준다.
+             }
     }
 }
