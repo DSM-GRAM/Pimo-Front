51ad2c3f231a15d16bca596eb1bd3486d092bb13
eggbee <ty39563956@gmail.com>
2017-11-07 19:18:34 +0900

    modify id

--- Pimo/Pimo/app/src/main/java/gram/com/pimo/MainActivity.java
+++ Pimo/Pimo/app/src/main/java/gram/com/pimo/MainActivity.java
@@ -1,13 +1,43 @@
 package gram.com.pimo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        // 스플래시 화면 띄우기
+        startActivity(new Intent(this, SplashActivity.class));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.singin);
+
+        initialize();  // 시간이 걸리는 작업 처리
+    }
+
+    /**
+     * 스플래시 표시하는 것과 초기화를 동시에 진행시키기 위하여 쓰레드 처리
+     *
+     */
+    private void initialize()
+    {
+        InitializationRunnable init = new InitializationRunnable();
+        new Thread(init).start();
+    }
+
+    /**
+     * 초기화 작업 처리
+     *
+     */
+    class InitializationRunnable implements Runnable
+    {
+        public void run()
+        {
+            // 여기서부터 초기화 작업 처리
+            // do_something
+        }
     }
 }
